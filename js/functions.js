9b9e9fcad8a50ac35145fa654eba54eab743f34e
Marcin Mazurek <marcin@mazurek.pro>
2017-03-07 12:52:36 +0100

    Add average fuel price fetching

--- js/functions.js
+++ js/functions.js
@@ -3,8 +3,10 @@ const DISTANCES_SELECTOR = '.section-directions-trip-distance > div:first-of-typ
 
 // TODO: set as configurable
 const FUEL_USAGE_PER_100_KM = 7;
-const FUEL_PRICE = 4.69;
 const CURRENCY = 'PLN';
+const LOCALE = 'pl-PL';
+
+const fuelType = { PB98: 2, PB95: 3, ON: 4, LPG: 5 };
 
 function observeChildChanges(container, onChange) {
   const observer = new MutationObserver(onChange);
@@ -25,22 +27,69 @@ function getDrivingTravelPlanContainer() {
 function injectFuelEstimate(plan) {
   const distances = plan.querySelectorAll(DISTANCES_SELECTOR);
 
-  distances.forEach(distance => {
-    const parsedDistance = parseFloat(distance.textContent.replace(',', ''));
-    const cost = calculateFuelUsage(parsedDistance);
-    distance.parentNode.appendChild(createFuelCostLabel(cost));
+  fetchFuelPrice().then(fuelPrice => {
+    distances.forEach(distance => {
+      const parsedDistance = parseFloat(distance.textContent.replace(',', ''));
+      const cost = calculateFuelUsage(parsedDistance, fuelPrice);
+      distance.parentNode.appendChild(createFuelCostLabel(cost));
+    });
   });
 }
+
+const formatter = new Intl.NumberFormat(LOCALE, { style: 'currency', currency: CURRENCY });
 
 function createFuelCostLabel(cost) {
   const fuelUsageElement = document.createElement('div');
   fuelUsageElement.style.marginTop = '3px';
-  fuelUsageElement.innerHTML = `Fuel cost: ${cost} zł`;
+  const formattedCost = formatter.format(cost);
+  fuelUsageElement.innerHTML = `Fuel cost: ${formattedCost}`;
   return fuelUsageElement;
 }
 
-function calculateFuelUsage(distance) {
-  return Math.round(distance * FUEL_USAGE_PER_100_KM/100 * FUEL_PRICE);
+function calculateFuelUsage(distance, fuelPrice) {
+  return Math.round(distance * FUEL_USAGE_PER_100_KM/100 * fuelPrice);
+}
+
+function fetchFuelPrice() {
+  return new Promise((resolve, reject) => {
+    const date = new Date();
+    const cacheKey = 'fuel_cost_calculator_' + date.getDay() + '-' + date.getMonth() + '-' + date.getFullYear();
+
+    try {
+      const cachedFuelPrice = localStorage.getItem(cacheKey);
+      if (cachedFuelPrice) {
+        resolve(Number(cachedFuelPrice));
+      }
+    } catch (e) {
+      // ignore local storage errors
+    }
+
+    fetch('https://www.e-petrol.pl/notowania/rynek-krajowy/ceny-stacje-paliw')
+      .then(response => response.text())
+      .then(text => {
+        const fuelPricesPage = document.createElement('div');
+        fuelPricesPage.innerHTML = text;
+        const fuelPriceList = fuelPricesPage.querySelector('#tablesort249 tbody tr:first-child');
+        let fuelPrice;
+
+        try {
+          const fuelPriceAsString = fuelPriceList.children[fuelType.PB95].textContent;
+          fuelPrice = parseFloat(fuelPriceAsString.replace(',', '.'));
+        } catch (e) {
+          reject(e);
+          return;
+        }
+
+        try {
+          localStorage.setItem(cacheKey, fuelPrice);
+        } catch (e) {
+          // ignore local storage errors
+        }
+
+        resolve(fuelPrice);
+      })
+      .catch(reject);
+  });
 }
 
 function watchTravelPlanChangesAndInjectFuelEstimate() {
